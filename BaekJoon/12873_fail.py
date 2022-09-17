559056e173331f922a0fbdc1f3286b3c3eab879f
asdf <88918799+ahffk333@users.noreply.github.com>
2022-09-17 13:39:23 +0900

    Add files via upload

---
+++ BaekJoon/12873_fail.py
@@ -0,0 +1,20 @@
+from collections import deque
+import sys
+n = int(sys.stdin.readline())
+queue = deque()
+a = 0
+
+
+for i in range(1, n+1):
+    queue.appendleft(i)
+
+
+
+while len(queue) != 1:
+    a += 1
+    b = a**3
+    for i in range(b % len(queue)):
+        queue.appendleft(queue.pop())
+    queue.pop()
+
+print(queue.pop())
