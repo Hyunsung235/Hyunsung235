8efddc1ee3e4831c7f92898e4a546e1325664cb4
Hyunsung235 <88918799+Hyunsung235@users.noreply.github.com>
2023-11-04 15:38:51 +0900

    Delete BaekJoon directory

--- BaekJoon/9461.py
+++
@@ -1,8 +0,0 @@
-import sys
-memory = [1, 1, 1, 2, 2]
-for i in range(int(sys.stdin.readline())):
-    a = int(sys.stdin.readline())
-    if a > len(memory) - 1:
-        for j in range(a - (len(memory) - 1)):
-            memory.append(memory[-1] + memory[len(memory) - 5])
-    print(memory[a - 1])
